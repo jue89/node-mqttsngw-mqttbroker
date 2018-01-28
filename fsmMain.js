c44eb36a4e7db98ba5684e5683e4740b547d5558
Jue <me@jue.yt>
2018-01-28 22:01:55 +0100

    Added main fsm

---
+++ fsmMain.js
@@ -0,0 +1,16 @@
+const FSM = require('edfsm');
+module.exports = (bus, log) => {
+	const clientFactory = require('./fsmClient.js')(bus, log);
+	return FSM({
+		fsmName: '[MQTTBroker] Main',
+		log: log,
+		input: bus,
+		output: bus,
+		firstState: 'listening'
+	}).state('listening', (ctx, i, o, next) => {
+		// Listen for broker connect messages
+		i(['brokerConnect', '*', 'req'], (data) => {
+			clientFactory.run(data);
+		});
+	});
+};
